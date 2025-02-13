0c0aca7acb97b529b647644a5b457119365d3df7
Onlyoneariom <ariom6696@gmail.com>
2025-02-13 19:02:20 +0100

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,15 @@
+first_name = "RACHAEL"
+last_name = "ONLYONEARIOM"
+full_name = first_name+last_name
+example = "Haa"*5
+
+print("First Name :", first_name)
+print("Last Name :", last_name)
+print("Full Name:", full_name)
+print("String Multipled 5 times gives this result :", example)
+
+word = 'Ariom'
+print("Length of string :", len(word))
+print("First Letter of String :", word[0])
+print("Last Letter of String :", word[4])
+print("string Sliced :", word[0:3])
