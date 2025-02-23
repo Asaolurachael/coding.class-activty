2d87af9d4fa261107aeec52c44596fa0b3a91ac1
Onlyoneariom <ariom6696@gmail.com>
2025-02-23 18:00:18 +0100

    Add files via upload

--- a1.py
+++ a1.py
@@ -1,15 +1,7 @@
-first_name = "RACHAEL"
-last_name = "ONLYONEARIOM"
-full_name = first_name+last_name
-example = "Haa"*5
-
-print("First Name :", first_name)
-print("Last Name :", last_name)
-print("Full Name:", full_name)
-print("String Multipled 5 times gives this result :", example)
-
-word = 'Ariom'
-print("Length of string :", len(word))
-print("First Letter of String :", word[0])
-print("Last Letter of String :", word[4])
-print("string Sliced :", word[0:3])
+n = int(input('enter a number, to find its factorial'))
+def fact(n):
+    if n == 0:
+        return 1
+    else:
+        return n*fact(n-1)
+print(fact(n))
